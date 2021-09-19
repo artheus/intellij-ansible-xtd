69a22ad9ba2f3010233a7b5d130688552d50606f
GitHub Action <action@github.com>
2021-09-19 10:05:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/artheus/intellijansiblextd/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.artheus.intellijansiblextd.services
+
+import com.github.artheus.intellijansiblextd.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
